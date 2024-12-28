f4bba95ebf6096998944b02c3907298f36e4a4eb
Темирлан <raiymbektemirlan@gmail.com>
2024-12-28 17:03:37 +0500

    Add pytz dependency

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ LOCAL_TZ = timezone("Asia/Aqtobe")
 API_TOKEN = "8152580581:AAEYHPMHBe1OnqGwmmBMbmNikboC_iIbtKc"
 
 # Укажите ID администратора
-ADMIN_ID = 1041578395  # Замените на ваш Telegram ID
+ADMIN_ID = 1313126991  # Замените на ваш Telegram ID
 
 # Инициализация логирования
 logging.basicConfig(level=logging.INFO)
